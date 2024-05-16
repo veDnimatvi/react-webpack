f75d42ddcdaac06030b2a76cdcd83e8a154115c4
codeninzza <codertom100@gmail.com>
2024-05-16 15:51:07 +0700

    optimization bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const VENDOR_LIBS = ["react", "react-dom"]; // những dependencies libs không thay đổi
 
 module.exports = {
-  entry: "./src/index.js", // Dẫn tới file index.js ta đã tạo
+  entry: {
+    bundle: "./src/index.js",
+    vendor: VENDOR_LIBS,
+  },
+  // Dẫn tới file index.js ta đã tạo, lần đầu vào trang web => tiến hành tải bandle.js và vendor.js về và lưu cache.
+  // Các lần sau bundle.js chứa các component nội dung thay đổi => luôn tải về để cập nhật lại nội dung, vendor: chứa các thư viện luôn giữ nguyên , lưu cache và lấy trong cache ra dùng.
   output: {
     path: path.join(__dirname, "/build"), // Thư mục chứa file được build ra
-    filename: "bundle.js", // Tên file được build ra
+    filename: "[name].js", // Tên file được build ra
   },
   module: {
     rules: [
@@ -32,11 +38,17 @@ module.exports = {
   },
   // Chứa các plugins sẽ cài đặt trong tương lai
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
     new MiniCssExtractPlugin({
       filename: "style.css",
     }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
   ],
+  // optimize để bundle.js không load các thư viện của vendor một lần nữa. gây nặng file bundle
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
 };
